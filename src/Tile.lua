52aad65f5499671f9f1c90647f9adbc78e1f7def
silviovielhauer <57265143+silviovielhauer@users.noreply.github.com>
2022-10-31 11:12:32 -0700

    Add files via upload

--- src/Tile.lua
+++ src/Tile.lua
@@ -1,12 +1,9 @@
 --[[
     GD50
     Match-3 Remake
-
     -- Tile Class --
-
     Author: Colton Ogden
     cogden@cs50.harvard.edu
-
     The individual tiles that make up our game board. Each Tile can have a
     color and a variety, with the varietes adding extra points to the matches.
 ]]
@@ -14,7 +11,7 @@
 Tile = Class{}
 
 function Tile:init(x, y, color, variety)
-    
+
     -- board positions
     self.gridX = x
     self.gridY = y
@@ -26,17 +23,31 @@ function Tile:init(x, y, color, variety)
     -- tile appearance/points
     self.color = color
     self.variety = variety
+
+    self.shiny = math.random(1, 10) == 10
 end
 
 function Tile:render(x, y)
-    
+
     -- draw shadow
     love.graphics.setColor(34, 32, 52, 255)
     love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
         self.x + x + 2, self.y + y + 2)
-
-    -- draw tile itself
-    love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.draw(gTextures['main'], gFrames['tiles'][self.color][self.variety],
-        self.x + x, self.y + y)
+      -- draw tile itself
+      if self.shiny then
+        -- multiply so drawing white rect makes it brighter
+        love.graphics.setBlendMode("add")
+        love.graphics.setColor(1, 1, 0.8, 120 / 255)
+        love.graphics.rectangle("line",
+          (self.gridX - 1) * 32 + (VIRTUAL_WIDTH - 272),
+          (self.gridY - 1) * 32 + 16,
+          32,
+          32,
+          4
+        )
+      -- back to alpha
+      love.graphics.setBlendMode("alpha")
+    end
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(gTextures["main"], gFrames["tiles"][self.color][self.variety], self.x + x, self.y + y)
 end
